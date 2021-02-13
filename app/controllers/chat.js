19f1d63bb647388389f6362b3aac123e3fc80c5f
lizandro kenedy <lisantos@mpmt.mp.br>
2021-02-13 16:56:55 -0400

    valições envio formulário

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,5 +1,16 @@
 const { application } = require("express");
 
 module.exports.iniciarChat = (application, req, res) => {
+    const dadosForm = req.body;
+    req.assert('apelido', 'O campo apelido é obrigatório').notEmpty()
+    req.assert('apelido', 'O campo apelido deve conter entre 3 e 15 caracteres').len(3, 15)
+
+    const erros = req.validationErrors();
+
+    if(erros){
+        res.render('index', {validacao: erros});
+        return
+    }
+
     res.render('chat');
 }
